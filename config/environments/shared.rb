9a3cbe2877e6c17285c73e635fe5f07145f0109f
Stephen Wu <wujstephen@gmail.com>
2017-11-27 21:17:19 -0500

    reorganized secrets & config vars for improved workflow

---
+++ config/environments/shared.rb
@@ -0,0 +1,16 @@
+# Shared environemnt config for development, production, and test
+
+Rails.application.configure do
+  # config for amazon S3 and paperclip, method 2 using dotenv direct
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => Rails.application.secrets.aws_bucket,
+      :access_key_id => Rails.application.secrets.aws_access_key_id,
+      :secret_access_key => Rails.application.secrets.aws_secret_access_key,
+      :s3_region => Rails.application.secrets.aws_s3_region
+    },
+    :url => ':s3_alias_url',
+    :s3_host_alias => Rails.application.secrets.aws_s3_host_alias
+  }
+end
