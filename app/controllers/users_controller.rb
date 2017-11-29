c45a5b00734d0b6e3ed076dc8099f9af3b28d3c5
Jason Magoto <magoto.14@buckeyemail.osu.edu>
2017-11-29 01:26:51 -0500

    Started to add gallery feature

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,13 @@
 # User Controller class
 #
-# Author:: Stephen
+# Author:: Stephen, Jason
 # Help:: https://auth0.com/docs/quickstart/webapp/rails/03-user-profile
 
 class UsersController < ApplicationController
   include Secured
   # GET /user/:id
   def show
-
+    @user = User.find(params[:id])
+    render :show
   end
 end
