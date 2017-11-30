ddb87a47edf7496430f9c5ced7f98e6ebfc37b05
Martin Yzeiri <yzeiri.1@osu.edu>
2017-11-30 13:47:52 -0500

    Added additional search controller tests

--- test/controllers/search_controller_test.rb
+++ test/controllers/search_controller_test.rb
@@ -1,10 +1,13 @@
+# Test cases for the advanced search feature
+#
+# Author(11/30/17):: Nishad
+# Update(11/30/17):: [Martin] Added additional search cases
 require 'test_helper'
 
 class SearchControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
-  
 
   test "get new successful" do
     xhr :get, :new, :format => :js
@@ -34,6 +37,55 @@ class SearchControllerTest < ActionController::TestCase
     assert_response :success
     assert_not_empty assigns(:wallpapers)
   end
+
+  test "adv_search successful on title and tag" do
+    post :adv_search, {title: "Wallpaper1", tags: "test1"}
+    assert_response :success
+    assert_not_empty assigns(:wallpapers)
+  end
+
+  test "adv_search successful on title and width" do
+    post :adv_search, {title: "Wallpaper1", width: 40}
+    assert_response :success
+    assert_not_empty assigns(:wallpapers)
+  end
+
+  test "adv_search successful on title and height" do
+    post :adv_search, {title: "Wallpaper1", height: 20}
+    assert_response :success
+    assert_not_empty assigns(:wallpapers)
+  end
+
+  test "adv_search successful on title, width, and height" do
+    post :adv_search, {title: "Wallpaper1", width: 40, height: 20}
+    assert_response :success
+    assert_not_empty assigns(:wallpapers)
+  end
+
+  test "adv_search successful on tag and width" do
+    post :adv_search, {tags: "test1", width: 1920}
+    assert_response :success
+    assert_not_empty assigns(:wallpapers)
+  end
+
+  test "adv_search successful on tag and height" do
+    post :adv_search, {tags: "test1", height: 20}
+    assert_response :success
+    assert_not_empty assigns(:wallpapers)
+  end
+
+  test "adv_search successful on tag, width, and height" do
+    post :adv_search, {tags: "test1", width: 40, height: 20}
+    assert_response :success
+    assert_not_empty assigns(:wallpapers)
+  end
+
+  test "adv_search successful on width and height" do
+    post :adv_search, {width: 40, height: 20}
+    assert_response :success
+    assert_not_empty assigns(:wallpapers)
+  end
+
   test "adv_search successful on all params" do
     post :adv_search, {title: "Wallpaper1", tags: "test1", image_height: 20, image_width: 40}
     assert_response :success
